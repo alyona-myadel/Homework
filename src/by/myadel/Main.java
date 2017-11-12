184f1a14f4efa0f5701b4baf8ca8871b122d9b8b
Alyona Myadel <alena.myadel@gmail.com>
2017-11-12 17:37:52 +0300

    Task 1

--- src/by/myadel/Main.java
+++ src/by/myadel/Main.java
@@ -3,6 +3,18 @@ package by.myadel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        task1();
+    }
+
+    private static void task1() {
+        System.out.println("Task 1:");
+        String string = "Эта строка поделена пополам";
+        int size = string.length();
+        System.out.println("Количество символов в строке = " + size);
+        int middleIndex = size / 2;
+        String stringLeft = string.substring(0, middleIndex);
+        String stringRight = string.substring(middleIndex, size);
+        System.out.println("Левая половина строки = " + stringLeft);
+        System.out.println("Правая половина строки = " + stringRight);
     }
 }

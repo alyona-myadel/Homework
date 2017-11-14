ee49332f7be9fda64a8fefaea5ac7820fe39e767
Alyona Myadel <alena.myadel@gmail.com>
2017-11-15 00:07:20 +0300

    Task 2.3

--- src/by/myadel/task2/Main.java
+++ src/by/myadel/task2/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         task1();
+        task3();
     }
 
     private static void task1() {
@@ -47,5 +48,42 @@ public class Main {
             }
         }
         System.out.print("]");
+    }
+
+    private static void task3() {
+        System.out.println("Task 3: \nВведите размер массива: ");
+        Scanner inputScanner = new Scanner(System.in);
+        int size = inputScanner.nextInt();
+        if (size <= 0) {
+            System.out.println("Error");
+            return;
+        }
+        float[] array = new float[size];
+        for (int i = 0; i < size; ++i) {
+            System.out.println("Введите целое число в массив (index = " + i + "):");
+            float valueArray = inputScanner.nextFloat();
+            array[i] = valueArray;
+        }
+        System.out.print("[");
+        for (int i = 0; i < size; ++i) {
+            if (i < size - 1) {
+                System.out.print(array[i] + ", ");
+            } else {
+                System.out.println(array[i] + "]");
+            }
+        }
+        for (int i = 0; i < size / 2; ++i) {
+            float temp = array[i];
+            array[i] = array[size - 1 - i];
+            array[size - 1 - i] = temp;
+        }
+        System.out.print("[");
+        for (int i = 0; i < size; ++i) {
+            if (i < size - 1) {
+                System.out.print(array[i] + ", ");
+            } else {
+                System.out.println(array[i] + "]");
+            }
+        }
     }
 }
